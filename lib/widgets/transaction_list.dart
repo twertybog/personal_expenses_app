bb1df0b93968aa36eb233a9bb85e7a8f8a0740f4
twertybog <twertybog@gmail.com>
2023-02-26 20:57:46 +0200

    make it possible to add transactions

---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../models/transaction.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+
+  TransactionList(this.transactions);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Card(
+              child: Row(
+            children: <Widget>[
+              Container(
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.blueAccent, width: 2)),
+                  padding: const EdgeInsets.all(10),
+                  child: Text(
+                    "\$${transactions[index].amount}",
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                        color: Colors.blueAccent),
+                  )),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(transactions[index].title,
+                      style: const TextStyle(
+                          fontSize: 17, fontWeight: FontWeight.bold)),
+                  Text(
+                    DateFormat().format(transactions[index].date),
+                    style: const TextStyle(color: Colors.blueGrey),
+                  )
+                ],
+              )
+            ],
+          ));
+        },
+        itemCount: transactions.length,
+      ),
+    );
+  }
+}
