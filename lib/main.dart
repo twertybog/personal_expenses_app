f3f7c7bd30ff450d442f709b3ce0e479931e9d09
twertybog <twertybog@gmail.com>
2023-02-27 14:22:10 +0200

    add fonts and new theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:personal_expenses_app/widgets/user_transaction.dart';
+import 'package:flutter/rendering.dart';
+import 'package:personal_expenses_app/widgets/transaction_list.dart';
+
+import 'models/transaction.dart';
+import 'widgets/new_transaction.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,20 +14,58 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Expenses',
+      theme: ThemeData(
+          primarySwatch: Colors.blueGrey,
+          primaryColorDark: Colors.black,
+          fontFamily: 'JosefinSans'),
       home: _MyHomePage(),
     );
   }
 }
 
-class _MyHomePage extends StatelessWidget {
+class _MyHomePage extends StatefulWidget {
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<_MyHomePage> {
   final titleController = TextEditingController();
   final amountController = TextEditingController();
+
+  final List<Transaction> _userTransactions = [
+    Transaction("t1", "New shoes", 69.99, DateTime.now()),
+    Transaction("t2", "Gym", 13.65, DateTime.now())
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+        DateTime.now().toString(), txTitle, txAmount, DateTime.now());
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (bctx) {
+          return GestureDetector(
+            onTap: () {},
+            behavior: HitTestBehavior.opaque,
+            child: NewTransaction(_addNewTransaction),
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Expenses'),
+        actions: <Widget>[
+          IconButton(
+              onPressed: () => _startAddNewTransaction(context),
+              icon: Icon(Icons.add))
+        ],
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
@@ -38,8 +80,12 @@ class _MyHomePage extends StatelessWidget {
               ),
             ),
           ),
-          UserTransaction(),
+          TransactionList(_userTransactions),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _startAddNewTransaction(context),
       ),
     );
   }
